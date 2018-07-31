a27d95bf6a90a6151057fde4cbaf5574553e059e
Charlie Zhang <czhang@wish.com>
2018-07-30 18:24:54 -0700

    Preload background image

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PreloadImage from './PreloadImage';
 import './Background.css';
 import backgroundsJSON from '../data/backgrounds.json';
 
@@ -17,16 +18,11 @@ class Background extends Component {
       <div>
         {
           this.state.backgrounds.length > 0 && this.state.backgrounds.map(background => {
-            return <div 
-              key={background.title}
+            return <PreloadImage key={background.title} 
+              srcPreload={background.thumbnailURL} 
+              srcLoaded={background.backgroundURL}
               className={this.props.activeBackground === background.title ? "Background Background-show" : "Background"} 
-              style={{
-                backgroundImage: `url(${background.backgroundURL})`,
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat"
-              }}>
-            </div>
+            />
           })
         }
         <div 
---
+++ src/components/PreloadImage.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './PreloadImage.css';
+
+class PreloadImage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.imageHD = null;
+  }
+
+  componentDidMount() {
+        
+    const loaderImg = new Image();
+
+    loaderImg.src = this.props.srcLoaded;
+
+    loaderImg.onload = () => {
+      this.imageHD.setAttribute(
+        'style',
+        `background-image: url('${this.props.srcLoaded}');
+        background-position: center;
+        background-size: cover;
+        background-repeat: no-repeat;`
+      );
+      this.imageHD.classList.add('PreloadImage-fade-in');
+    }
+
+  };
+
+  render() {
+    return (
+      <div className={"PreloadImage-container " + this.props.className}>
+        <div 
+          className="PreloadImage-loaded" 
+          ref={imageLoadedElem => this.imageHD = imageLoadedElem}>
+        </div>
+        <div 
+          className="PreloadImage-preload" 
+          style={{ 
+            backgroundImage: `url('${this.props.srcPreload}')`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat"
+          }}>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default PreloadImage;
