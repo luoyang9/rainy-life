1bc212b2b66327bc77481b65b8a958b3aa8d7a48
Charlie Zhang <luoyang9@gmail.com>
2018-08-05 15:36:35 -0700

    Finish about panel

--- src/components/AboutPanel.js
+++ src/components/AboutPanel.js
@@ -9,9 +9,19 @@ class AboutPanel extends Component {
 
     return (
       <div className={"AboutPanel " + className}>
-        <i onClick={this.props.toggleAbout} class="material-icons AboutPanel-close">close</i>
-        <h1 className="AboutPanel-header">rainy life</h1>
-        <h3 className="AboutPanel-subheader">rain simulator for the heart and soul</h3>
+        <div className="AboutPanel-content">
+          <i onClick={this.props.toggleAbout} className="material-icons AboutPanel-close">close</i>
+          <h1 className="AboutPanel-header">rainy life</h1>
+          <h3 className="AboutPanel-subheader">rain simulator for rain lovers</h3>
+          <p className="AboutPanel-description">welcome to rainy life, a website dedicated to the soothing sound of rain.
+          sit back, relax, and enjoy a peaceful moment.</p>
+          <h3 className="AboutPanel-subheader">features</h3>
+          <p className="AboutPanel-feature"><span>autosave: </span>rainy life will remember your currently active background, sounds, and youtube link</p>
+          <p className="AboutPanel-feature"><span>link sharing: </span>rainy life can generate a custom URL for you to share your favourite set up</p>
+          <h3 className="AboutPanel-subheader" style={{marginTop: 30}}>comments & suggestions</h3>
+          <p className="AboutPanel-suggestion">email me at <a href="mailto:rainylifewebsite@gmail.com" target="_blank">rainylifewebsite@gmail.com</a></p>
+          <p className="AboutPanel-footer">built with <span className="AboutPanel-heart">♥</span> by charlie</p>
+        </div>
       </div>
     );  
   }
